048ce094d2aecd82b3e7507c7a19424cd911f54a
Ahsan <91184500+ahsfar@users.noreply.github.com>
2023-07-03 22:15:52 +0800

    Update index.php
    commenting

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <div class="center-align">
 	<ul>
 		<div class="left-align">
-			<li><a href="read.php"><strong>Contact Us</strong></a></li>
+<!-- 			<li><a href="read.php"><strong>Contact Us</strong></a></li> -->
 			<li><a href="insert.php"><strong>Send Message</strong></a></li>
-			<li><a href="update.php"><strong>Show Messages</strong></a></li>
-			<li><a href="delete.php"><strong>Remove a Message</strong></a></li>
+<!-- 			<li><a href="update.php"><strong>Show Messages</strong></a></li>
+			<li><a href="delete.php"><strong>Remove a Message</strong></a></li> -->
 		</div>
 	</ul>
 </div>
